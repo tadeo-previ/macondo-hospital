971ba163400a9804daddc671a430420381c46ea9
tadeo@uwaterloo.ca <gatrod67@gmail.com>
2026-08-01 13:58:56 -0500

    Implement SQL safety check and enhance UI instructions
    Added SQL safety check and updated Streamlit UI to inform users about the database tables.

--- hm_app.py
+++ hm_app.py
@@ -73,6 +73,7 @@ def run_sql_query(query):
 st.set_page_config(page_title="AI SQL Assistant")
 
 st.title("Hospital de Macondo - AI SQL Assistant")
+st.write("The hospital database has 4 tables: Patients, Appoinments, Hopspital_Records, Lab_Results, Outpatient_Visits")
 st.write("Ask a question in plain English, and I’ll help you turn it into SQL.")
 st.write("""HOW TO USE THIS ASSISTANT: 
 1. Type a plain-English question about the data; 
