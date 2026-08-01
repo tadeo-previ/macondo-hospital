ca568d88d50648df974f82f216bd5e9bba976c1d
tadeo@uwaterloo.ca <gatrod67@gmail.com>
2026-08-01 15:52:41 -0500

    Implement SQL safety check and enhance UI
    Added SQL safety check and updated UI for user prompts.

--- hm_app.py
+++ hm_app.py
@@ -91,7 +91,7 @@ st.write("""HOW TO USE THIS ASSISTANT:
 """)
 
 with st.form("user_question_form"):
-    user_question = st.text_input("What would you like to know about your data?...Enter prompts as an SQLite database is used")
+    user_question = st.text_input("What would you like to know about your data?...Enter prompts as it's used on an SQLite database")
     submitted = st.form_submit_button("Generate SQL")
 
 if submitted:
